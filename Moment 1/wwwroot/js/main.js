93911fc8cc44cf47cb61fc4755f163a437938f76
Malin Larsson <mala1812@student.miun.se>
2020-01-31 11:47:57 +0100

    Add project files.

---
+++ Moment 1/wwwroot/js/main.js
@@ -0,0 +1,10 @@
+﻿"use strict"
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("closebtn").style.display = "block";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("closebtn").style.display = "none";
+}
